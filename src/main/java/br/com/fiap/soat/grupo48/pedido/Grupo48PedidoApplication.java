c3ccd2fa3cbec85643d37768111e1d8e337386f7
André Felipe Laus <andre.felipe.laus@gmail.com>
2024-05-13 23:15:52 -0300

    Migração das classes de produto

---
+++ src/main/java/br/com/fiap/soat/grupo48/pedido/Grupo48PedidoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.soat.grupo48.pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo48PedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo48PedidoApplication.class, args);
+	}
+
+}
